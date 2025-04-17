ae1871342463c722691d52fbd4eddec6d02a0d75
yasss2627 <yyabogoss@gmail.com>
2025-04-17 15:27:55 +0200

    AAAAA

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello World!");
+    return res.send("Hello World! For the second time.");
 })
 
 app.get('/api', function (req, res) {
